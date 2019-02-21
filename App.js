6a8d0db99e6f0de8047271b3f4701d6a4ed671fc
giuliobosco <giuliobva@gmail.com>
2019-02-21 21:52:32 +0100

    fix App Image

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Matteo Bosco's APP!</Text>
-      </View>
-    );
-  }
+	render() {
+		let pic = {
+			uri: 'http://giuliobosco.ch/i.jpg'
+		};
+		return (
+			<View style={styles.container}>
+				<Text>Matteo Bosco's APP!</Text>
+				<Image source={pic} style={{width:217, height: 387}}/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-      fontSize: '30px'
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	/*image: {
+		width: 217,
+		height: 387,
+	},*/
 });
