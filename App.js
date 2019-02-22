93022c70a76f1c315e27c5f99660849b261f810d
giuliobosco <giuliobva@gmail.com>
2019-02-22 09:56:40 +0100

    fix justifyContent center

--- App.js
+++ App.js
@@ -7,11 +7,12 @@ export default class App extends Component {
 			<View style={{
 				flex:1,
 				flexDirection: 'colum',
-				justifyContent: 'space-between'
+				justifyContent: 'center',
+				alignItems: 'stretch'
 			}}>
 				<View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}/>
-				<View style={{width: 50, height: 50, backgroundColor: 'skyblue'}}/>
-				<View style={{width: 50, height: 50, backgroundColor: 'steelblue'}}/>
+				<View style={{height: 50, backgroundColor: 'skyblue'}}/>
+				<View style={{height: 100, backgroundColor: 'steelblue'}}/>
 			</View>
 		);
 	}
