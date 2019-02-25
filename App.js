a0917c2a551543005d92bb1a82bf3a2348729305
giuliobosco <giuliobva@gmail.com>
2019-02-25 17:29:01 +0100

    fix App - notifications

--- App.js
+++ App.js
@@ -1,55 +1,123 @@
 import React, {Component} from 'react';
 import {
-	FlatList,
-	ActivityIndicator,
+	StyleSheet,
 	Text,
-	View
+	View,
+	TextInput,
+	Modal,
+	TouchableOpacity
 } from 'react-native';
+import {Permissions, Notifications} from 'expo';
 
 export default class App extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {isLoading: true}
-	}
+	state = {
+		notification: null,
+		messageText: ''
+	};
+
+	registerForPushNotificationsAsync = async () => {
+		const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+		if (status !== 'granted') {
+			console.log(status) ;
+			return;
+		}
+		let token = await Notifications.getExpoPushTokenAsync();
+		return fetch(PUSH_REGISTRATION_ENDPOINT, {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				token: {
+					value: token,
+				},
+				user: {
+					username: 'warly',
+					name: 'Dan Ward'
+				},
+			}),
+		});
+
+		this.notificationSubscription = Notifications.addListener(this.handleNotification);
+	};
+
+	handleNotification = (notification) => {
+		this.setState({notification});
+	};
+
+	handleChangeText = (text) => {
+		this.setState({messageText: text});
+	};
 
 	componentDidMount() {
-		return fetch('https://facebook.github.io/react-native/movies.json')
-			.then((response) => response.json())
-			.then((responseJson) => {
-
-				this.setState({
-					isLoading: false,
-					dataSource: responseJson.movies,
-				}, function () {
-
-				});
-
-			})
-			.catch((error) => {
-				console.error(error);
-			});
+		this.registerForPushNotificationsAsync();
 	}
+
+	sendMessage = async () => {
+		fetch(MESSAGE_ENPOINT, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				message: this.state.messageText,
+			}),
+		});
+		this.setState({messageText: ''});
+	};
 
 
 	render() {
-
-		if (this.state.isLoading) {
-			return (
-				<View style={{flex: 1, padding: 20, paddingTop:50}}>
-					<ActivityIndicator/>
-				</View>
-			)
-		}
-
 		return (
-			<View style={{flex: 1, padding: 20, paddingTop: 50}}>
-				<FlatList
-					data={this.state.dataSource}
-					renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
-					keyExtractor={({id}, index) => id}
+			<View style={styles.container}>
+				<TextInput
+					value={this.state.messageText}
+					onChangeText={this.handleChangeText}
+					style={styles.textInput}
 				/>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={this.sendMessage}
+				>
+					<Text style={styles.buttonText}>Send</Text>
+				</TouchableOpacity>
+				{this.state.notification ?
+					this.renderNotification()
+					: null}
 			</View>
 		);
 	}
 }
+
+const PUSH_REGISTRATION_ENDPOINT = 'http://71f549cc.ngrok.io/token';
+const MESSAGE_ENPOINT = 'http://71f549cc.ngrok.io/message';
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#474747',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	textInput: {
+		height: 50,
+		width: 300,
+		borderColor: '#f6f6f6',
+		borderWidth: 1,
+		backgroundColor: '#fff',
+		padding: 10
+	},
+	button: {
+		padding: 10
+	},
+	buttonText: {
+		fontSize: 18,
+		color: '#fff'
+	},
+	label: {
+		fontSize: 18
+	}
+});
